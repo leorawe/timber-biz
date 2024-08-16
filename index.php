b2804bf8ae978b70787228fa6962a7ec2c7d8a4e
Leora Wenger <leorawenger@gmail.com>
2024-08-16 14:03:29 -0400

    header, menu, wrapper, menu li css.

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
 
 $context          = Timber::context();
 $context['posts'] = Timber::get_posts();
-$context['foo']   = '';
+// $context['foo']   = '';
+$context['page'] = 'home';
 $templates        = array( 'index.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
