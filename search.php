3d7029aca8317ab8d4f21ccec8ee2966fd887429
Leora Wenger <leorawenger@gmail.com>
2024-08-20 15:53:37 -0400

    no results, posts-container 80px.

--- search.php
+++ search.php
@@ -15,5 +15,6 @@ $context          = Timber::context();
 $context['title'] = 'Search results for: ' . get_search_query();
 $context['sidebar_1'] = Timber::get_widgets('sidebar-1');
 $context['posts'] = Timber::get_posts();
+$context['results'] = count($context['posts']);
 
 Timber::render( $templates, $context );
