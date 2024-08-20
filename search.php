cb2c1b84340ba9dc0d7713ae1427ee3c07ef2a0d
Leora Wenger <leorawenger@gmail.com>
2024-08-20 15:36:16 -0400

    search results, widget styling, footer menu.

--- search.php
+++ search.php
@@ -12,7 +12,8 @@
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 
 $context          = Timber::context();
-$context['title'] = 'Search results for ' . get_search_query();
+$context['title'] = 'Search results for: ' . get_search_query();
+$context['sidebar_1'] = Timber::get_widgets('sidebar-1');
 $context['posts'] = Timber::get_posts();
 
 Timber::render( $templates, $context );
